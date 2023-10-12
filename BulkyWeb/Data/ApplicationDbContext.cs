864efecf88c1dce02e47ad6116dbca716e4eb2c4
RillSvai <kirilkvas07@gmail.com>
2023-10-12 15:05:31 +0300

    add creating, some speaking with db and validation

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) 
+        {
+                
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(new Category[]
+            {
+                new () {Id = 1,DisplayOrder = 1, Name = "Action" },
+                new () {Id = 2,DisplayOrder = 2, Name = "SciFi" },
+                new () {Id = 4,DisplayOrder = 4, Name = "History"},
+                new () {Id = 3, DisplayOrder = 3, Name = "Horror"}
+            });
+        }
+    }
+}
