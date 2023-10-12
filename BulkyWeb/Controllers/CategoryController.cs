cf4f401f1f87da9e7ff9646fba31b14ca031a32b
RillSvai <kirilkvas07@gmail.com>
2023-10-12 17:48:04 +0300

    fixed bugs

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -27,11 +27,59 @@ namespace BulkyWeb.Controllers
         {
             if (ModelState.IsValid) 
             {
+                TempData["success"] = "Category created successfully!";
 				_db.Categories.Add(obj);
 				_db.SaveChanges();
 				return RedirectToAction("Index", "Category");
 			}
             return View();
         }
-    }
+		public IActionResult Edit(int? id)
+		{
+            if (id == null) 
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.FirstOrDefault(category => category.Id == id);
+            if (category == null) 
+            {
+                return NotFound();
+            }
+			return View(category);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (ModelState.IsValid)
+			{
+				TempData["success"] = "Category updated successfully!";
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index", "Category");
+			}
+			return View();
+		}
+		public IActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+			Category? category = _db.Categories.FirstOrDefault(category => category.Id == id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+		[HttpPost]
+		public IActionResult Delete(Category obj)
+		{
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+			TempData["success"] = "Category deleted successfully!";
+			return RedirectToAction("Index", "Category");
+		}
+
+	}
 }
