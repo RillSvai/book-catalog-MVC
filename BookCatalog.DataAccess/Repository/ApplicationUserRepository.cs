f9f04a548be931e198b20bd79645855f8d5e6c73
RillSvai <kirilkvas07@gmail.com>
2023-11-20 15:41:39 +0200

    added ApplicationUser Repo

---
+++ BookCatalog.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using BookCatalog.DataAccess.Data;
+using BookCatalog.DataAccess.Repository.IRepository;
+using BookCatalog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCatalog.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
